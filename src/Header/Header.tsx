1d871a18b9bc958a6c22159087f974ed374ea80b
Olegoriy <olegkib1506@gmail.com>
2025-03-25 18:45:13 +0300

    fix: header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,18 +1,23 @@
+import React from 'react';
 import './Header.scss';
 
-const Header = () => {
+const Header: React.FC = () => {
     return (
         <header className='header'>
-            <img src="/logo.svg" alt="Logo" className="header__logo"/>
+            <img 
+                src="/logo.svg" 
+                alt="Logo" 
+                className="header__logo"
+            />
             <a 
                 href="mailto:Appleoleg2005@mail.ru?subject=Hello from Portfolio&body=Hi there!%0D%0A%0D%0AI wanted to say..." 
                 className="header__btn header__btn-mail"
                 aria-label="Send email"
             >
-            SAY HELLO
+                SAY HELLO
             </a>
         </header>
-    )
-}
+    );
+};
 
 export default Header;
